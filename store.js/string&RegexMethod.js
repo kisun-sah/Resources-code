7397589a20689f24f09a84dd28c5d33ceb47b63f
kisun-sah <kishunsah783@gmail.com>
2024-02-21 18:47:21 +0530

    In this session we cover string , stringmethod , Regex

---
+++ store.js/string&RegexMethod.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+//Templet string
+
+const tem ={name:"Iphone",brand:"Apple",price:120000};
+
+const str = `The name of the product is ${tem.name} which belong to ${tem.brand} brand with a price ${tem.price}`;
+
+console.log(str);
